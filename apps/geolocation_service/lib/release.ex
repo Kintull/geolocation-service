16d700d7babed1c51774ae9656c4eac0e580c944
Roman Berdichevskii <r378ut@gmail.com>
2023-09-17 22:08:34 +0200

    added migrator to heroku

---
+++ apps/geolocation_service/lib/release.ex
@@ -0,0 +1,24 @@
+defmodule GeolocationService.Release do
+  def migrate do
+    ensure_started()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    ensure_started()
+
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp ensure_started do
+    Application.ensure_all_started(:ssl)
+  end
+
+  defp repos do
+    Application.load(:geolocation_service)
+    Application.fetch_env!(:geolocation_service, :ecto_repos)
+  end
+end
